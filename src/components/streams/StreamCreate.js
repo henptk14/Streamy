9461c0899b29224fd3a5e9d4d8ab3fc39ab8feda
Pyi Thein Khine <37124353+henptk14@users.noreply.github.com>
2019-10-22 18:12:58 -0700

    259

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,84 @@
-import React from "react";
+import React, { Component } from "react";
+import { Field, reduxForm } from "redux-form";
+import { connect } from "react-redux";
+import { createStream } from "../../actions";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
+export class StreamCreate extends Component {
+  renderInput = formProps => {
+    console.log(formProps);
+    const className = `field ${
+      formProps.meta.error && formProps.meta.touched ? "error" : ""
+    }`;
+    return (
+      <div className={className}>
+        <label>{formProps.label}</label>
+        <input {...formProps.input} autoComplete="off" />
+        {this.renderError(formProps.meta)}
+      </div>
+    );
+  };
+
+  renderError = ({ error, touched }) => {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
+  };
+
+  onSubmit = formValues => {
+    this.props.createStream(formValues);
+  };
+
+  render() {
+    //console.log(this.props);
+    return (
+      <div>
+        <form
+          className="ui form error"
+          onSubmit={this.props.handleSubmit(this.onSubmit)}
+        >
+          <Field
+            name="title"
+            component={this.renderInput}
+            label="Enter Title"
+          />
+          <Field
+            name="description"
+            component={this.renderInput}
+            label="Enter Description"
+          />
+          <button className="ui button primary">Submit</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+const Validate = formValues => {
+  const errors = {};
+  if (!formValues.title) {
+    // only ran if the user did not enter a title
+    errors.title = "You must enter a title";
+  }
+  if (!formValues.description) {
+    // only ran if the user did not enter a description
+    errors.description = "You must enter a description";
+  }
+
+  return errors;
 };
 
-export default StreamCreate;
+const formWrapped = reduxForm({
+  form: "streamCreate",
+  validate: Validate
+})(StreamCreate);
+
+export default connect(
+  null,
+  {
+    createStream
+  }
+)(formWrapped);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import reduxThunk from "redux-thunk";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from "./components/App";
+import reducers from "./ruducers";
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+  reducers,
+  composeEnhancer(applyMiddleware(reduxThunk))
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
